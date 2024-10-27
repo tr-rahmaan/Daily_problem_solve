8e35286851dbb8bb2f85944b7ed3dc8af7be6883
tr-rahmaan <trrahmaan@gmail.com>
2024-10-28 01:55:21 +0600

    Add files via upload
    solved

--- Week_1/Day_2/G - How many.cpp
+++ Week_1/Day_2/G - How many.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  long long s,p,cnt=0;
+  cin>>s>>p;
+  for(int i=0;i<=s;i++){
+    for(int j=0;j<=s;j++){
+        for(int k=0;k<=s;k++){
+            if(i+j+k<=s){
+                if(i*k*j<=p) cnt+=1;
+            }
+        }
+    }
+  }
+
+  cout<<cnt<<'\n';
+
+return 0;
+}
 
