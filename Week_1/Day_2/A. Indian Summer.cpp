948ff73452efafbe60d5d67db233ceee6ea1bdf5
tr-rahmaan <trrahmaan@gmail.com>
2024-10-28 20:20:42 +0600

    Add files via upload
    solved

---
+++ Week_1/Day_2/A. Indian Summer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+     vector<pair<string,string>>p(n);
+    for(int i=0;i<n;i++){
+        cin>>p[i].first>>p[i].second;
+    }
+
+    sort(p.begin(),p.end());
+    int siz=unique(p.begin(),p.end())-p.begin();
+    cout<<siz<<endl;
+  return 0;
+}
